dd7025f56ebcad371ea4e7dcc58325ada7aed41e
Sm3llin <michaelellin@gmail.com>
2018-04-09 22:03:06 +1000

    React-Router and Bootstrap Sketch

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,105 +1,20 @@
-require("module").globalPaths.push(__dirname + "/node_modules");
-require("source-map-support/source-map-support.js").install();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const electron = require('electron');
-let { Menu } = electron.remote;
+import App from './components/App';
+import HelloApp from './components/HelloApp';
+import PageNotFound from "./components/PageNotFound";
 
-require('./main.css');
-document.getElementById('app').innerHTML = `
-<div id="screenTerminal">
-  <p id="terminalOutput">
-  </p>
-</div>
-<div id="screenFooter">
-  <form onsubmit="event.preventDefault()">
-    <button id="terminalSubmit">Send</button>
-    <div style="overflow: hidden;">
-      <label for="terminalInput"></label><input id="terminalInput" type="text">
-    </div>
-  </form>
-</div>`;
+import "bootstrap/dist/css/bootstrap.css";
 
-import { TerminalInput, TerminalOutput } from './components/terminal/terminal';
-import SerialSession from './serial';
+ReactDOM.render((
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={App}/>
+        <Route exact path='/hello' component={HelloApp}/>
+        <Route component={PageNotFound}/>
+      </Switch>
+    </BrowserRouter>
+), document.getElementById('app'));
 
-let input = new TerminalInput(document.getElementById('terminalInput'));
-let output = new TerminalOutput( document.getElementById('terminalOutput'));
-
-let session = new SerialSession();
-
-session.on('data', (data) => {output.add(data)});
-session.on('open', () => {output.add("--------------------\nConnected . . . \n--------------------\n")});
-session.on('close', () => {input.disable();output.add("--------------------\nDisconnected . . . \n--------------------\n")});
-
-document.getElementById('terminalSubmit').addEventListener('click', () => {
-  if (session.isOpen()) {
-    output.add('<span style="color: red">' + input.getValue() + '</span>\n');
-    session.send(input.getValue());
-
-    input.clear();
-    output.moveTo();
-  }
-});
-
-let menuTemplate = [
-  {
-    label: 'Connect',
-    click: () => {
-      input.enable();
-      session.startSession((err) => {
-        console.error(err);
-        alert(err);
-        input.disable();
-      });
-    }
-  },
-  {
-    label: 'Disconnect',
-    click: () => {
-      session.close();
-    }
-  },
-  {
-    label: 'Settings',
-    submenu: [
-      {
-        label: 'Baud Rate',
-        submenu: [
-          {
-            label: '9600',
-            click: () => {
-              session.set('baudRate', 9600)
-            }
-          },
-          {
-            label: '115200',
-            click: () => {
-              session.set('baudRate', 115200)
-            }
-          }
-        ]
-      }
-    ]
-  }
-];
-
-session.on('ready', () => {
-  let portOptions = [];
-  session.ports.forEach((port) => {
-      portOptions.push({
-        label: port,
-        click() {
-          session.set('comName', port)
-        }
-      });
-    });
-
-  menuTemplate[2].submenu.push({
-    label: 'Serial Port',
-    submenu: portOptions
-  });
-
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
-});
-
-Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
